696460617e14f310cff25c2a5bd9292b30942ef5
mamama888 <mamama.888www@gmail.com>
2020-09-30 17:56:21 +0900

    最初２

---
+++ easylogin/src/main/java/com/example/easylogin/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.easylogin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EasyloginApplication.class);
+	}
+
+}
